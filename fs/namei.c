f04e98aa60b0853cd3be507ddc568a5ca49895a2
Miklos Szeredi <mszeredi@suse.cz>
2014-07-23 15:15:30 +0200

    BACKPORT: fs: call rename2 if exists
    Christoph Hellwig suggests:
    
    1) make vfs_rename call ->rename2 if it exists instead of ->rename
    2) switch all filesystems that you're adding NOREPLACE support for to
       use ->rename2
    3) see how many ->rename instances we'll have left after a few
       iterations of 2.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    @acroreiser: backport to 3.10, fuse changes omitted

--- fs/namei.c
+++ fs/namei.c
@@ -3438,10 +3438,11 @@ static int vfs_rename_dir(struct vfsmount *mnt,
 
 	if (target)
 		shrink_dcache_parent(new_dentry);
-	if (!flags) {
+	if (!old_dir->i_op->rename2) {
 		error = old_dir->i_op->rename(old_dir, old_dentry,
 					      new_dir, new_dentry);
 	} else {
+		WARN_ON(old_dir->i_op->rename != NULL);
 		error = old_dir->i_op->rename2(old_dir, old_dentry,
 					       new_dir, new_dentry, flags);
 	}
@@ -3481,10 +3482,11 @@ static int vfs_rename_other(struct inode *old_dir, struct dentry *old_dentry,
 	if (d_mountpoint(old_dentry)||d_mountpoint(new_dentry))
 		goto out;
 
-	if (!flags) {
+	if (!old_dir->i_op->rename2) {
 		error = old_dir->i_op->rename(old_dir, old_dentry,
 					      new_dir, new_dentry);
 	} else {
+		WARN_ON(old_dir->i_op->rename != NULL);
 		error = old_dir->i_op->rename2(old_dir, old_dentry,
 					       new_dir, new_dentry, flags);
 	}
@@ -3525,6 +3527,7 @@ int vfs_rename2(struct vfsmount *mnt,
 	if (error)
 		return error;
 
+<<<<<<< HEAD
 	if (!old_dir->i_op->rename)
 		
 #ifdef CONFIG_GOD_MODE
@@ -3535,6 +3538,10 @@ return -EPERM;
 #ifdef CONFIG_GOD_MODE
 }
 #endif
+=======
+	if (!old_dir->i_op->rename && !old_dir->i_op->rename2)
+		return -EPERM;
+>>>>>>> c960d0bb480 (BACKPORT: fs: call rename2 if exists)
 
 	if (flags && !old_dir->i_op->rename2)
 		return -EINVAL;
@@ -3580,6 +3587,7 @@ SYSCALL_DEFINE5(renameat2, int, olddfd, const char __user *, oldname,
 	if (IS_ERR(from)) {
 		error = PTR_ERR(from);
 		goto exit;
+	}
 
 	error = user_path_parent(newdfd, newname, &newnd, &to);
 	if (error)
