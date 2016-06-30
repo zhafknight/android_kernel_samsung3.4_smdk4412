c1d5d32f8d6499aa88994e0747a9f56b23de5d7e
Martin KaFai Lau <kafai@fb.com>
2016-06-30 10:28:42 -0700

    cgroup: Add cgroup_get_from_fd
    Add a helper function to get a cgroup2 from a fd.  It will be
    stored in a bpf array (BPF_MAP_TYPE_CGROUP_ARRAY) which will
    be introduced in the later patch.
    
    Change-Id: I3e52c90e9e5fd9b8f65e8b0e0aeccaf25a131f5a
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Cc: Alexei Starovoitov <ast@fb.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Tejun Heo <tj@kernel.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -745,6 +745,8 @@ struct cgroup_subsys_state *cgroup_css_from_dir(struct file *f, int id);
 void cgroup_sk_alloc(struct cgroup **skcg);
 void cgroup_sk_clone(struct cgroup *skcg);
 void cgroup_sk_free(struct cgroup *skcg);
+
+struct cgroup *cgroup_get_from_fd(int fd);
 #else /* !CONFIG_CGROUPS */
 
 static inline int cgroup_init_early(void) { return 0; }
