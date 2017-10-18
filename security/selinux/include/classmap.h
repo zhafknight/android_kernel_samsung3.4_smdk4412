e113ee92a0654cfc21d158e54c77d9165282cca6
Chenbo Feng <fengc@google.com>
2017-10-18 13:00:25 -0700

    UPSTREAM: selinux: bpf: Add selinux check for eBPF syscall operations
    Implement the actual checks introduced to eBPF related syscalls. This
    implementation use the security field inside bpf object to store a sid that
    identify the bpf object. And when processes try to access the object,
    selinux will check if processes have the right privileges. The creation
    of eBPF object are also checked at the general bpf check hook and new
    cmd introduced to eBPF domain can also be checked there.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Reviewed-by: James Morris <james.l.morris@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (cherry-pick from net-next: ec27c3568a34c7fe5fcf4ac0a354eda77687f7eb)
    Bug: 30950746
    Change-Id: Ifb0cdd4b7d470223b143646b339ba511ac77c156
    
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    Change-Id: I073b5ebe76a280267289357af2b5d8f3afcaffa4

--- security/selinux/include/classmap.h
+++ security/selinux/include/classmap.h
@@ -155,5 +155,7 @@ struct security_class_mapping secclass_map[] = {
 	{ "kernel_service", { "use_as_override", "create_files_as", NULL } },
 	{ "tun_socket",
 	  { COMMON_SOCK_PERMS, NULL } },
+	{ "binder", { "impersonate", "call", "set_context_mgr", "transfer", NULL } },
+	{ "bpf", { "map_create", "map_read", "map_write", "prog_load", "prog_run" } },
 	{ NULL }
   };
