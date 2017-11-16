6dd27fa9f5be1ef3500bda2f41e4ce41babd0d65
Marissa Wall <marissaw@google.com>
2017-11-16 14:32:40 -0800

    BACKPORT: USB: core: harden cdc_parse_cdc_header
    Andrey Konovalov reported a possible out-of-bounds problem for the
    cdc_parse_cdc_header function.  He writes:
    	It looks like cdc_parse_cdc_header() doesn't validate buflen
    	before accessing buffer[1], buffer[2] and so on. The only check
    	present is while (buflen > 0).
    
    So fix this issue up by properly validating the buffer length matches
    what the descriptor says it is.
    
    (cherry picked from commit 2e1c42391ff2556387b3cb6308b24f6f65619feb)
    
    (The original patch fixed the generic cdc_parser_cdc_header function.
    That generic function did not exist in 3.10 but there are a couple
    cdc parsers that suffer from the same underlying problem.)
    
    Bug: 69052594
    Change-Id: Ib251469de39e51b0ed7c1a1b88873270afccd90f
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Marissa Wall <marissaw@google.com>
    CVE-2017-16534
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -923,6 +923,17 @@ static int acm_probe(struct usb_interface *intf,
 	}
 
 	while (buflen > 0) {
+		if ((buflen < buffer[0]) || (buffer[0] < 3)) {
+			dev_err(&intf->dev, "invalid descriptor buffer length\n");
+			break;
+		}
+
+		elength = buffer[0];
+		if (!elength) {
+			dev_err(&intf->dev, "skipping garbage byte\n");
+			elength = 1;
+			goto next_desc;
+		}
 		if (buffer[1] != USB_DT_CS_INTERFACE) {
 			dev_err(&intf->dev, "skipping garbage\n");
 			goto next_desc;
