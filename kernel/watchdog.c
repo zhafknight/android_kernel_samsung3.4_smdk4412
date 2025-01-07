7ff7b594efea4dff1d24422ea723cb7002e1456d
html6405 <peter.schelchshorn@mhs-solutions.at>
2025-01-07 19:56:15 +0100

    Revert "watchdog: HACK: don't panic on CPU hard lockup"
    This reverts commit 88a3ad23e4188992f9afc5cbf57970a8130b4668.

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -231,13 +231,10 @@ static void watchdog_check_hardlockup_other_cpu(void)
 		if (per_cpu(hard_watchdog_warn, cpu) == true)
 			return;
 
-		/*
 		if (hardlockup_panic)
 			panic("Watchdog detected hard LOCKUP on cpu %d", cpu);
 		else
 			WARN(1, "Watchdog detected hard LOCKUP on cpu %d", cpu);
-		*/
-		pr_emerg("Watchdog detected hard LOCKUP on cpu %d", cpu);
 
 		per_cpu(hard_watchdog_warn, cpu) = true;
 	} else {
@@ -295,12 +292,10 @@ static void watchdog_overflow_callback(struct perf_event *event,
 		if (__this_cpu_read(hard_watchdog_warn) == true)
 			return;
 
-		pr_emerg("Watchdog detected hard LOCKUP on cpu %d", cpu);
-		/*
 		if (hardlockup_panic)
 			panic("Watchdog detected hard LOCKUP on cpu %d", this_cpu);
 		else
-			WARN(1, "Watchdog detected hard LOCKUP on cpu %d", this_cpu);*/
+			WARN(1, "Watchdog detected hard LOCKUP on cpu %d", this_cpu);
 
 		__this_cpu_write(hard_watchdog_warn, true);
 		return;
