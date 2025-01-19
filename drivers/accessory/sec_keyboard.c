ce75acdc09facb946adea9c3953a64f98bcbecd6
html6405 <peter.schelchshorn@mhs-solutions.at>
2025-01-19 20:01:40 +0100

    sec_keyboard.c: add debugging messages

--- drivers/accessory/sec_keyboard.c
+++ drivers/accessory/sec_keyboard.c
@@ -342,6 +342,7 @@ static void keyboard_fb_suspend(struct sec_keyboard_drvdata *data)
 		if (data->univ_kbd_dock == false)
 			sec_keyboard_tx(data, 0x10);	/* the idle mode */
 	}
+    printk(KERN_DEBUG "[Keyboard] %s\n", __func__);
 	data->fb_suspended = true;
 }
 
@@ -352,7 +353,7 @@ static void keyboard_fb_resume(struct sec_keyboard_drvdata *data)
 
 	if (data->kl != UNKOWN_KEYLAYOUT)
 		printk(KERN_DEBUG "[Keyboard] %s\n", __func__);
-
+    printk(KERN_DEBUG "[Keyboard] %s\n", __func__);
     data->fb_suspended = false;
 }
 
