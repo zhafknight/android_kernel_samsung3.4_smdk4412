cfb809aa10b0411c83a8c644b8de31c9a4cb3c4e
Yaroslav Zviezda <acroreiser@gmail.com>
2025-04-15 23:29:49 +0300

    HACK: dcache: do not BUG() if we have dentry->d_count == 1

--- fs/dcache.c
+++ fs/dcache.c
@@ -983,7 +983,8 @@ static void shrink_dcache_for_umount_subtree(struct dentry *dentry)
 				       dentry->d_count,
 				       dentry->d_sb->s_type->name,
 				       dentry->d_sb->s_id);
-				BUG();
+
+				dentry->d_count--;
 			}
 
 			if (IS_ROOT(dentry)) {
