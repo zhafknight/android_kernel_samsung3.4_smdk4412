721104e60a8604937f372f7d38658ecde262c874
Keenan Dong <keenanat2000@gmail.com>
2026-04-08 16:46:00 +0800

    BACKPORT: rtmutex: Use waiter::task instead of current in remove_waiter()
    remove_waiter() is used by the slowlock paths, but it is also used for
    proxy-lock rollback in rt_mutex_start_proxy_lock() when invoked from
    futex_requeue().
    
    In the latter case waiter::task is not current, but remove_waiter()
    operates on current for the dequeue operation. That results in several
    problems:
    
      1) the rbtree dequeue happens without waiter::task::pi_lock being held
    
      2) the waiter task's pi_blocked_on state is not cleared, which leaves a
         dangling pointer primed for UAF around.
    
      3) rt_mutex_adjust_prio_chain() operates on the wrong top priority waiter
         task
    
    Use waiter::task instead of current in all related operations in
    remove_waiter() to cure those problems.
    
    [ tglx: Fixup rt_mutex_adjust_prio_chain(), add a comment and amend the
      	changelog ]
    
    Fixes: 8161239a8bcc ("rtmutex: Simplify PI algorithm and make highest prio task get lock")
    Reported-by: Yuan Tan <yuantan098@gmail.com>
    Reported-by: Yifan Wu <yifanwucs@gmail.com>
    Reported-by: Juefei Pu <tomapufckgml@gmail.com>
    Reported-by: Xin Liu <bird@lzu.edu.cn>
    Signed-off-by: Keenan Dong <keenanat2000@gmail.com>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    
    [
     @acroreiser: backport to 3.4, switch kernel/rtmutex.c to gnu99 to avoid build error below.
    
    include/linux/cleanup.h:112:9: error: 'for' loop initial declarations are only allowed in C99 or C11 mode
      112 |         for (CLASS(_name, scope)(args),
    ]

--- kernel/rtmutex.c
+++ kernel/rtmutex.c
@@ -486,19 +486,23 @@ static void wakeup_next_waiter(struct rt_mutex *lock)
  *
  * Must be called with lock->wait_lock held and
  * have just failed to try_to_take_rt_mutex().
+ *
+ * When invoked from rt_mutex_start_proxy_lock() waiter::task != current !
  */
 static void remove_waiter(struct rt_mutex *lock,
 			  struct rt_mutex_waiter *waiter)
 {
 	int first = (waiter == rt_mutex_top_waiter(lock));
 	struct task_struct *owner = rt_mutex_owner(lock);
+	struct task_struct *waiter_task = waiter->task;
+	struct rt_mutex *next_lock = NULL;
 	unsigned long flags;
 	int chain_walk = 0;
 
-	raw_spin_lock_irqsave(&current->pi_lock, flags);
-	plist_del(&waiter->list_entry, &lock->wait_list);
-	current->pi_blocked_on = NULL;
-	raw_spin_unlock_irqrestore(&current->pi_lock, flags);
+	scoped_guard(raw_spinlock_irqsave, &waiter_task->pi_lock) {
+		plist_del(&waiter->list_entry, &lock->wait_list);
+		waiter_task->pi_blocked_on = NULL;
+	}
 
 	if (!owner)
 		return;
