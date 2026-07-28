e0688761d829dcfc7517583be8ddaa99388f4f9e
zhafknight <zhafmazran@gmail.com>
2026-07-28 11:02:50 +0800

    mach-u1.c: gate thermal throttle points on CONFIG_EXYNOS4210_1200MHZ_SUPPORT

--- arch/arm/mach-exynos/mach-u1.c
+++ arch/arm/mach-exynos/mach-u1.c
@@ -7501,19 +7501,33 @@ static struct platform_device *smdkc210_devices[] __initdata = {
 #ifdef CONFIG_EXYNOS4_SETUP_THERMAL
 /* below temperature base on the celcius degree */
 struct s5p_platform_tmu u1_tmu_data __initdata = {
-	.ts = { //use 4x12 values -gm
-		.stop_1st_throttle  = 78,
-		.start_1st_throttle = 80,
+	.ts = {
+#ifdef CONFIG_EXYNOS4210_1200MHZ_SUPPORT
+		.stop_1st_throttle  = 61,
+		.start_1st_throttle = 64,
 		.stop_2nd_throttle  = 87,
 		.start_2nd_throttle = 103,
 		.start_tripping     = 110,
 		.start_emergency    = 120,
+#else
+		.stop_1st_throttle  = 59,
+		.start_1st_throttle = 64,
+		.stop_2nd_throttle  = 87,
+		.start_2nd_throttle = 103,
+		.start_tripping     = 110,
+		.start_emergency    = 120,
+#endif
 		.stop_mem_throttle  = 80,
 		.start_mem_throttle = 85,
 	},
 	.cpufreq = {
+#ifdef CONFIG_EXYNOS4210_1200MHZ_SUPPORT
 		.limit_1st_throttle  = 800000, /* 800MHz in KHz order */
 		.limit_2nd_throttle  = 200000, /* 200MHz in KHz order */
+#else
+		.limit_1st_throttle  = 1000000, /* 1000MHz in KHz order */
+		.limit_2nd_throttle  =  500000, /*  500MHz in KHz order */
+#endif
 	},
 };
 #endif
