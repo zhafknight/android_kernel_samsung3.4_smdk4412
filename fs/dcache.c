ea88bce791e4b503abf76bfc1993a83db21482aa
j0sh1x <aljoshua.hell@gmail.com>
2026-08-08 22:50:43 +0200

    Revert "ion,mm,fs: stop logspam"
    This reverts commit 67ac85b3a06df4864100705d98dbf0cbc9c802f2.

--- fs/dcache.c
+++ fs/dcache.c
@@ -972,7 +972,6 @@ static void shrink_dcache_for_umount_subtree(struct dentry *dentry)
 			__d_shrink(dentry);
 
 			if (dentry->d_count != 0) {
-/*
 				printk(KERN_ERR
 				       "BUG: Dentry %p{i=%lx,n=%s}"
 				       " still in use (%d)"
@@ -984,7 +983,7 @@ static void shrink_dcache_for_umount_subtree(struct dentry *dentry)
 				       dentry->d_count,
 				       dentry->d_sb->s_type->name,
 				       dentry->d_sb->s_id);
-*/
+
 				dentry->d_count--;
 			}
 
