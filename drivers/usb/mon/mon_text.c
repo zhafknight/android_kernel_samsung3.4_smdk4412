4d8e0d2715246c3e6a633d8064df17a5b0c29fb3
j0sh1x <aljoshua.hell@gmail.com>
2026-08-09 17:44:47 +0200

    Revert "USB: usbmon: remove assignment from IS_ERR argument"
    This reverts commit b2cc37cdbfcec4d4dc5cd478f3c544283f563bdc.

--- drivers/usb/mon/mon_text.c
+++ drivers/usb/mon/mon_text.c
@@ -385,8 +385,7 @@ static ssize_t mon_text_read_t(struct file *file, char __user *buf,
 	struct mon_event_text *ep;
 	struct mon_text_ptr ptr;
 
-	ep = mon_text_read_wait(rp, file);
-	if (IS_ERR(ep))
+	if (IS_ERR(ep = mon_text_read_wait(rp, file)))
 		return PTR_ERR(ep);
 	mutex_lock(&rp->printf_lock);
 	ptr.cnt = 0;
@@ -413,8 +412,7 @@ static ssize_t mon_text_read_u(struct file *file, char __user *buf,
 	struct mon_event_text *ep;
 	struct mon_text_ptr ptr;
 
-	ep = mon_text_read_wait(rp, file);
-	if (IS_ERR(ep))
+	if (IS_ERR(ep = mon_text_read_wait(rp, file)))
 		return PTR_ERR(ep);
 	mutex_lock(&rp->printf_lock);
 	ptr.cnt = 0;
